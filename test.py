c5aac4c20828819fdbdd0ef39a7b0df70af44652
Nikola <stevanoskinikolaa@gmail.com>
2022-09-26 19:31:46 +0200

    changes

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-import time, os, sys, json
-from telethon import TelegramClient, sync
+import json
+from telethon import TelegramClient
 
 api_id = 9312723
 api_hash = 'e99afd73a5e31cb8285fd224d0370026';
@@ -19,21 +19,18 @@ api_hash = 'e99afd73a5e31cb8285fd224d0370026';
 # client = TelegramClient('x2', api_id, api_hash)
 
 client = TelegramClient('xxx', api_id, api_hash).start()
-
 users = [];
 
 def get_comments(client: TelegramClient, channel: str, message_id: int):
     async def crawl_comments():
         async for message in client.iter_messages(channel, reply_to=message_id):
-            # print(message.text)  # only comment
             full_comment_obj = message.to_dict()  # in JSON-Format
-            # print(full_comment_obj)
-            # y = json.dumps(message.from_id.to_dict());
-
             users.append(message.from_id.to_dict());
+
+            print(full_comment_obj)
 
     with client:
        client.loop.run_until_complete(crawl_comments())
 
 get_comments(client, 'whip347', 74053)
-print(json.dumps(users));
+# print(json.dumps(users)); 
