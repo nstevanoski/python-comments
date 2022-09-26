3d3157bbc4a5f39c252a2dd39eca7ff654258d5f
Nikola <stevanoskinikolaa@gmail.com>
2022-09-26 20:05:09 +0200

    Changes

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import json
+import random
 from telethon import TelegramClient
 
 api_id = 9312723
@@ -21,16 +22,28 @@ api_hash = 'e99afd73a5e31cb8285fd224d0370026';
 client = TelegramClient('xxx', api_id, api_hash).start()
 users = [];
 
-def get_comments(client: TelegramClient, channel: str, message_id: int):
+def getComments(client: TelegramClient, channel: str, message_id: int):
     async def crawl_comments():
         async for message in client.iter_messages(channel, reply_to=message_id):
             full_comment_obj = message.to_dict()  # in JSON-Format
             users.append(message.from_id.to_dict());
-
-            print(full_comment_obj)
+            # print(full_comment_obj)
 
     with client:
        client.loop.run_until_complete(crawl_comments())
 
-get_comments(client, 'whip347', 74053)
+def sendMessage(client: TelegramClient):
+    async def sMessage():
+        messages = ["Hello, How are you?", "Hi, What's up?", "Hey, do you want to gotrained?"]
+        await client.send_message('me', random.choice(messages))
+
+    with client:
+       client.loop.run_until_complete(sMessage())
+
+# Call functions
+
+getComments(client, 'whip347', 74053);
+sendMessage(client);
 # print(json.dumps(users)); 
+
+
